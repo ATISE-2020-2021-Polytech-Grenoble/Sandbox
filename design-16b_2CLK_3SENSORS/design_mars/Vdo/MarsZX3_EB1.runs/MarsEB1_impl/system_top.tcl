598f40a62aaf4f09be4020a0728fb977fbcd35ee
Pareilleux Killian <killian.pareilleux@etu.unilim.fr>
2021-03-01 10:02:33 +0100

    FreeRTOS code working, still with SD card

--- design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.runs/MarsEB1_impl/system_top.tcl
+++ design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.runs/MarsEB1_impl/system_top.tcl
@@ -88,26 +88,26 @@ start_step init_design
 set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
-  set_param chipscope.maxJobs 3
+  set_param chipscope.maxJobs 2
   create_project -in_memory -part xc7z020clg484-1
   set_property design_mode GateLvl [current_fileset]
   set_param project.singleFileAddWarning.threshold 0
-  set_property webtalk.parent_dir C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/design_mars/Vdo/MarsZX3_EB1.cache/wt [current_project]
-  set_property parent.project_path C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/design_mars/Vdo/MarsZX3_EB1.xpr [current_project]
+  set_property webtalk.parent_dir /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.cache/wt [current_project]
+  set_property parent.project_path /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.xpr [current_project]
   set_property ip_repo_paths {
-  C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/ips/HDPyx_DDR_To_AXI_zybo
-  C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/ips/HDPyx_TPG
+  /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/ips/HDPyx_DDR_To_AXI_zybo
+  /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/ips/HDPyx_TPG
 } [current_project]
   update_ip_catalog
-  set_property ip_output_repo C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/design_mars/Vdo/MarsZX3_EB1.cache/ip [current_project]
+  set_property ip_output_repo /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.cache/ip [current_project]
   set_property ip_cache_permissions {read write} [current_project]
   set_property XPM_LIBRARIES {XPM_CDC XPM_FIFO XPM_MEMORY} [current_project]
-  add_files -quiet C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/design_mars/Vdo/MarsZX3_EB1.runs/MarsEB1_synth/system_top.dcp
+  add_files -quiet /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.runs/MarsEB1_synth/system_top.dcp
   set_msg_config -source 4 -id {BD 41-1661} -limit 0
   set_param project.isImplRun true
-  add_files C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/design_mars/Vdo/MarsZX3_EB1.srcs/sources_1/bd/MarsZX3/MarsZX3.bd
+  add_files /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/design_mars/Vdo/MarsZX3_EB1.srcs/sources_1/bd/MarsZX3/MarsZX3.bd
   set_param project.isImplRun false
-  read_xdc C:/FMa/ATISE/Development/design-16b_2CLK_3SENSORS_modif/design_mars/src/MarsZX3_EB1.xdc
+  read_xdc /home/killian/Bureau/Atise/Sandbox/design-16b_2CLK_3SENSORS/design_mars/src/MarsZX3_EB1.xdc
   set_param project.isImplRun true
   link_design -top system_top -part xc7z020clg484-1
   set_param project.isImplRun false
